fe0f910d6c1281be44ac090a69213975eacef4f2
demo835 <demo835@yahoo.com>
2018-08-21 09:34:38 -0400

    updated App.js

---
+++ client/src/components/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+            </div>
+        )
+    }
+}
+
+export default App
