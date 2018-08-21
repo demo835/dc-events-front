2ec71d7bcb7ecf4cb3d2b585343e220b6711a994
demo835 <demo835@yahoo.com>
2018-08-21 15:35:58 -0400

    added stuff

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -9,7 +9,25 @@ class App extends Component {
   constructor() {
     super()
 
-    this.state = {}
+    this.state = {
+        
+    }
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3001/show")
+    .then(res => res.json())
+    .then(data => {
+        console.log("data is")
+        console.log(data)
+        // let items = []
+        // for(let i = 0; i < data.length; i++)
+        // {
+        //     items[i] = data[i]
+        // }
+        this.setState({response: data[0].condition})
+        this.setState({id: data[0]._id})
+    })
   }
 
   render() {
