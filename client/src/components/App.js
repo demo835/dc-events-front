e7bf2b7de175e4f33a9399ff7c37d4b360b1dd80
demo835 <demo835@yahoo.com>
2018-08-21 14:07:48 -0400

    updated Event

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Events from './Events'
 
 class App extends Component {
     constructor() {
--- client/src/components/Event.js
+++ client/src/components/Event.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 class Event extends Component {
     render () {
       return (
         <div>
-          
+            <div className="row">
+                <div className="col s12 m7">
+                <div className="card">
+                    <div className="card-image">
+                    <img src="../public/images/hamilton-1.jpg" alt="Hamilton"></img>
+                    <span className="card-title">Card Title</span>
+                    </div>
+                    <div className="card-content">
+                    <p>I am a very simple card. I am good at containing small bits of information.
+                    I am convenient because I require little markup to use effectively.</p>
+                    </div>
+                    <div className="card-action">
+                    <a href="#">This is a link</a>
+                    </div>
+                </div>
+                </div>
+            </div>
         </div>
       )
     }
