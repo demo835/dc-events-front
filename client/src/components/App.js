9b6b6c2b2075afc78e5b6e8083a4e770237689ac
demo835 <demo835@yahoo.com>
2018-08-21 12:21:21 -0400

    front end renders

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -12,6 +12,7 @@ class App extends Component {
     render() {
         return (
             <div>
+                <h1>DC Events Front End</h1>
             </div>
         )
     }
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App/App.js';
+import App from './components/App.js';
 import {BrowserRouter as Router} from 'react-router-dom'
 
 ReactDOM.render(
