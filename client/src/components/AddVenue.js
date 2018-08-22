a21c7729820f837688de124276b79974409a956d
Boku <Boku@NHs-MacBook-Pro.local>
2018-08-22 16:46:57 -0400

    got it

--- client/src/components/AddVenue.js
+++ client/src/components/AddVenue.js
@@ -7,89 +7,89 @@ class AddEvent extends Component {
     this.state = {};
   }
 
-  render() {
-    return (
-      <div>
-        <h1>Add Venue</h1>
-        <h3>
-          <Link to="/">Back to Events</Link>
-        </h3>
-        <form onSubmit={this.onSubmit}>
-          <div>
-            <label for="name">Name</label>
-            <input
-              type="text"
-              name="name"
-              value={name}
-              onChange={this.onChange}
-              placeholder="Name"
-            />
-          </div>
-          <div>
-            <label for="img">Img Url</label>
-            <input
-              type="text"
-              name="img"
-              value={img}
-              onChange={this.onChange}
-              placeholder="https://i.imgur.com/example.png"
-            />
-          </div>
-          <div>
-            <label for="name">Description</label>
-            <input
-              type="text"
-              name="description"
-              value={description}
-              onChange={this.onChange}
-              placeholder="Description"
-            />
-          </div>
-          <div>
-            <label for="name">Food</label>
-            <input
-              type="text"
-              name="food"
-              value={food}
-              onChange={this.onChange}
-              placeholder="Food"
-            />
-          </div>
-          <div>
-            <label for="name">Date</label>
-            <input
-              type="text"
-              name="date"
-              value={date}
-              onChange={this.onChange}
-              placeholder="##/##/####"
-            />
-          </div>
-          <div>
-            <label for="name">Cost</label>
-            <input
-              type="text"
-              name="cost"
-              value={cost}
-              onChange={this.onChange}
-              placeholder="Cost"
-            />
-          </div>
-          <div>
-            <label for="name">Venue</label>
-            <input
-              type="text"
-              name="venue"
-              value={venue}
-              onChange={this.onChange}
-              placeholder="Venue"
-            />
-          </div>
-          <button type="submit">Submit</button>
-        </form>
-      </div>
-    );
-  }
+  // render() {
+  //   return (
+  //     <div>
+  //       <h1>Add Venue</h1>
+  //       <h3>
+  //         <Link to="/">Back to Events</Link>
+  //       </h3>
+  //       <form onSubmit={this.onSubmit}>
+  //         <div>
+  //           <label for="name">Name</label>
+  //           <input
+  //             type="text"
+  //             name="name"
+  //             value={name}
+  //             onChange={this.onChange}
+  //             placeholder="Name"
+  //           />
+  //         </div>
+  //         <div>
+  //           <label for="img">Img Url</label>
+  //           <input
+  //             type="text"
+  //             name="img"
+  //             value={img}
+  //             onChange={this.onChange}
+  //             placeholder="https://i.imgur.com/example.png"
+  //           />
+  //         </div>
+  //         <div>
+  //           <label for="name">Description</label>
+  //           <input
+  //             type="text"
+  //             name="description"
+  //             value={description}
+  //             onChange={this.onChange}
+  //             placeholder="Description"
+  //           />
+  //         </div>
+  //         <div>
+  //           <label for="name">Food</label>
+  //           <input
+  //             type="text"
+  //             name="food"
+  //             value={food}
+  //             onChange={this.onChange}
+  //             placeholder="Food"
+  //           />
+  //         </div>
+  //         <div>
+  //           <label for="name">Date</label>
+  //           <input
+  //             type="text"
+  //             name="date"
+  //             value={date}
+  //             onChange={this.onChange}
+  //             placeholder="##/##/####"
+  //           />
+  //         </div>
+  //         <div>
+  //           <label for="name">Cost</label>
+  //           <input
+  //             type="text"
+  //             name="cost"
+  //             value={cost}
+  //             onChange={this.onChange}
+  //             placeholder="Cost"
+  //           />
+  //         </div>
+  //         <div>
+  //           <label for="name">Venue</label>
+  //           <input
+  //             type="text"
+  //             name="venue"
+  //             value={venue}
+  //             onChange={this.onChange}
+  //             placeholder="Venue"
+  //           />
+  //         </div>
+  //         <button type="submit">Submit</button>
+  //       </form>
+  //     </div>
+  //   );
+  // }
 }
 
 export default AddEvent;
