5d24c7bf0a9c481298fa603602ef53d1cc02a878
demo835 <demo835@yahoo.com>
2018-08-23 10:05:55 -0400

    fixed up navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,20 +1,22 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import "./NavBar.css"
 
 class NavBar extends Component {
   render() {
     return (
       <nav>
-        <div className="nav-wrapper #42a5f5 blue lighten-1">
+        <div className="nav-wrapper #0d47a1 blue darken-4">
+        <img src="../../images/bald-eagle-1.png" id="mascot" alt="mascot"></img>
           <a href="#" className="brand-logo">
             <Link to="/">DC Events</Link>
           </a>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
             <li>
-              <Link to="/add-event">+Event</Link>
+              <a className="#d32f2f red darken-2 waves-effect waves-light btn"><Link to="/add-event">+Event</Link></a>
             </li>
             <li>
-              <Link to="/venue/new">+Venue</Link>
+            <a className="#d32f2f red darken-2 waves-effect waves-light btn"><Link to="/venue/new">+Venue</Link></a>
             </li>
           </ul>
         </div>
